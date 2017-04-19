22d01c86f81c48c41ab91ced61b140b3e330b463
shawnrd <schuldinerchuckbilly@gmail.com>
2017-04-19 16:33:55 +0530

    slider

---
+++ js/code.js
@@ -0,0 +1,10 @@
+$(function(){
+        $('.slick-slider').slick({
+            autoplay: true,
+            autoplaySpeed: 3000,
+            dots: true,
+            arrows: true,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        });
+});
